89d8bd7a2021a54cb88f05c7d900b4aa1896bd19
Brian Paulsen <paul0836@d.umn.edu>
2018-03-17 12:41:11 -0500

    Finished valid function, but still has bugs

--- Solver.py
+++ Solver.py
@@ -1,4 +1,35 @@
-def valid(puzzle):
+#put in the puzzle and the coordinates of the newly added number
+def valid(puzzle, row, col):
+
+    #check rows
+    used = [False]*10
+    for i in range(9):
+        if puzzle[row][i] != 0:
+            if used[puzzle[row][i]] is True:
+                return False
+            used[puzzle[row][i]] = True
+
+    #check columns
+    used = [False]*10
+    for i in range(9):
+        if puzzle[i][col] != 0:
+            if used[puzzle[i][col]] is True:
+                return False
+            used[puzzle[i][col]] = True
+
+    #check boxes
+    top_corner_row = (row // 3) * 3
+    top_corner_col = (col // 3) * 3
+    used = [False]*10
+    for i in range(3):
+        for j in range(3):
+            curr = puzzle[top_corner_row + i][top_corner_col + j]
+            if curr != 0:
+                if used[curr] is True:
+                    return False
+                used[curr] = True
+
+    return True
 
 
 def solver(puzzle, start_row = 0, start_col = 0):
@@ -8,17 +39,14 @@ def solver(puzzle, start_row = 0, start_col = 0):
 
     if puzzle[start_row][start_col] == 0:
         for i in range(1, 10):
-            print(i)
             puzzle[start_row][start_col] = i
-            if valid(puzzle):
+            if valid(puzzle, start_row, start_col):
                 if solver(puzzle, start_row+1 if start_col == 8 else start_row, 0 if start_col == 8 else start_col+1):
                     return True
         return False
 
     else:
-        solver(puzzle, start_row + 1 if start_col == 8 else start_row, 0 if start_col == 8 else start_col + 1)
-        return True
-
+        return solver(puzzle, start_row + 1 if start_col == 8 else start_row, 0 if start_col == 8 else start_col + 1)
 
 
 def main():
@@ -32,7 +60,7 @@ def main():
         print(str(i) + ": ", end="")
         puzzle[i-1] = list(map(int, input().split()))
 
-    solver(puzzle)
+    print(solver(puzzle))
     for row in puzzle:
         for col in row:
             print(col, end=" ")
